b0de2ff04461d3713d5b79d28e0e95ec8b1c3477
AKLarrotta <agata.kotorowicz@gmail.com>
2018-08-12 12:03:25 +0200

    remote web driver

--- src/test/java/pl/lait/selenium1/Init.java
+++ src/test/java/pl/lait/selenium1/Init.java
@@ -1,10 +1,13 @@
 package pl.lait.selenium1;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class Init {
 	
@@ -12,13 +15,21 @@ public class Init {
 	
 	public static WebDriver getDriver() {
 		System.setProperty("webdriver.gecko.driver" , "C:\\Geckodriver\\geckodriver.exe");
-        
-        DesiredCapabilities cap = DesiredCapabilities.firefox();
+		URL url = null;
+		try {
+			url = new URL("http://192.168.0.110:4444/wd/hub");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+        DesiredCapabilities cap = DesiredCapabilities.chrome();
         if (driver == null) {
-        driver = new FirefoxDriver(cap);
+        //driver = new FirefoxDriver(cap);
+        driver = new RemoteWebDriver(url, cap);
         driver.get("http://newtours.demoaut.com");
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS); //maxymalny czas oczekiwania
-        driver.manage().window().maximize();//otwiera pełne okno
+        //driver.manage().window().maximize();//otwiera pełne okno
         return driver;
         }
         else {
